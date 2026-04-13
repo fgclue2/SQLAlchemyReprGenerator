70c09880f0e37f21e4db5f902a52bd272d649823
fgclue <lost@biitle.nl>
2026-04-13 19:30:18 -0300

    Add README and boilerplate feature

--- README.md
+++ README.md
@@ -1,71 +1,15 @@
-# sqlalchemyrepr README
+# SQLAlchemy \_\_repr\_\_() generator
 
-This is the README for your extension "sqlalchemyrepr". After writing up a brief description, we recommend including the following sections.
+This simple extension creates the `__repr__()` string for SQLAlchemy. SQLAlchemy's ORM requires you to create `__repr__()` functions and this extension generates the string for you:
 
-## Features
+![Showcase](video.gif)
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+## How to do the thing
 
-For example if there is an image subfolder under your extension project workspace:
+1. Press `Ctrl+Shift+P` to open the command pallete.
+2. Type `SQLAlchemy: Create __repr__() string`
+3. Type in your class name
+4. Type in your parameters that aren't relationships separated by commas
+5. The result will be inserted in your editor
 
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
-
-## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
-
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
----
-
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
-
-## For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+There is also the full boilerplate command, `SQLAlchemy: Create full __repr()__ boilerplate`, which creates the `__repr__()` function too.
--- src/extension.ts
+++ src/extension.ts
@@ -1,42 +1,54 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('sqlalchemyrepr.reprgenerate', () => {
-		const classInputBox = vscode.window.createInputBox();
-		classInputBox.title = "SQLAlchemy __repr__() Generator (1/2)";
-		classInputBox.prompt = "Type in your class name";
-		classInputBox.placeholder = "GenericTableClass";
+function magic(boilerplate: boolean = false) {
+	const classInputBox = vscode.window.createInputBox();
+	classInputBox.title = "SQLAlchemy __repr__() Generator (1/2)";
+	classInputBox.prompt = "Type in your class name";
+	classInputBox.placeholder = "GenericTableClass";
 
-		const paramInputBox = vscode.window.createInputBox();
-		paramInputBox.title = "SQLAlchemy __repr__() Generator (2/2)";
-		paramInputBox.prompt = "Type in your parameters separated by commas";
-		paramInputBox.placeholder = "id, name, email";
+	const paramInputBox = vscode.window.createInputBox();
+	paramInputBox.title = "SQLAlchemy __repr__() Generator (2/2)";
+	paramInputBox.prompt = "Type in your parameters separated by commas";
+	paramInputBox.placeholder = "id, name, email";
 
-		classInputBox.onDidAccept(() => {
-			classInputBox.hide();
-			paramInputBox.show();
-			vscode.window.showInformationMessage(classInputBox.value);
-		});
-
-		paramInputBox.onDidAccept(() => {
-			paramInputBox.hide();
-			
-			const value = paramInputBox.value.split(",").map((value) => value.trim());
-			const className = classInputBox.value.trim();
-			
-			const result = className + "(" + value.map(
-				(value) => value + "={self." + value + "!r}"
-			).join(", ") + ")";
-
-			vscode.window.showInformationMessage("Done! Inserted " + result + " into your text editor.");
-			vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(result));
-		});
-		
-		classInputBox.show();
+	classInputBox.onDidAccept(() => {
+		classInputBox.hide();
+		paramInputBox.show();
 	});
 
-	context.subscriptions.push(disposable);
+	paramInputBox.onDidAccept(() => {
+		paramInputBox.hide();
+
+		const value = paramInputBox.value.split(",").map((value) => value.trim());
+		const className = classInputBox.value.trim();
+
+		const result = className + "(" + value.map(
+			(value) => value + "={self." + value + "!r}"
+		).join(", ") + ")";
+
+		vscode.window.showInformationMessage("Done! Inserted " + result + " into your text editor.");
+		if(boilerplate) {
+			vscode.window.activeTextEditor?.insertSnippet(
+				new vscode.SnippetString("def __repr__(self) -> str:\n\t" + "return f\"" + result + "\"")
+			);
+		} else {
+			vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(result));
+		}
+	});
+
+	classInputBox.show();
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	const simple = vscode.commands.registerCommand('sqlalchemyrepr.reprgenerate', () => {
+		magic();
+	});
+	const boiler = vscode.commands.registerCommand('sqlalchemyrepr.boilergenerate', () => {
+		magic(true);
+	});
+
+	context.subscriptions.push(boiler);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
